a610b15bd0637c1e971109b705e513cb872e098e
Luffy <luisjaperez0106@gmail.com>
2025-10-31 14:35:54 -0500

    config: yazi with git
    config final of yazi at least for now

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,2 +1,11 @@
-require("full-border"):setup()
-require("git"):setup()
+require("full-border"):setup() --Ronded border
+-- Git config
+th.git = th.git or {}
+--modified file 
+th.git.modified = ui.Style():fg("blue") 
+th.git.modified_sign = "M"
+--deleted file
+th.git.deleted_sign = "D"
+th.git.deleted = ui.Style():fg("red"):bold()
+
+require("git"):setup({})
