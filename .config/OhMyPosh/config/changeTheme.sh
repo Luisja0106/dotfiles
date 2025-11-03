a7ca8e1ceaee4f7da054a7cfe09223f2a8650cac
Luffy <luisjaperez0106@gmail.com>
2025-11-03 15:29:55 -0500

    feat: add feature to change the oh my posh theme whith a script
    Bases for the future change Color scheme rice general

---
+++ .config/OhMyPosh/config/changeTheme.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+THEME_NAME="$(cat ~/.config/OhMyPosh/Themes/themes.txt)"
+export DIRECTORY="$HOME/.config/OhMyPosh/Themes"
+PALLETE="$(cat ~/.config/OhMyPosh/Themes/themes/theme.txt)"
+export PALLETE_DIR="$HOME/.config/OhMyPosh/Themes/themes/"
+COLORS="$PALLETE_DIR/$PALLETE"
+THEME="$DIRECTORY/$THEME_NAME"
+TEMPFILE="$DIRECTORY/currentTheme.toml"
+rm "$TEMPFILE"
+touch "$TEMPFILE"
+cat "$COLORS" > "$TEMPFILE"
+cat "$THEME" >> "$TEMPFILE"
+source ~/.zshrc
