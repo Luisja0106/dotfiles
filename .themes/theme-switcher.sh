0666d2b7adaedc9733b1827c98803f19064ef590
Luffy <luisjaperez0106@gmail.com>
2025-11-07 17:57:59 -0500

    add color scheme swicher
    add a color scheme swicher whit tokyo and nord themes

---
+++ .themes/theme-switcher.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+#INFO: paths
+OMP="$HOME/.config/OhMyPosh/Themes/themes"
+NVIM="$HOME/.config/nvim/lua"
+YAZI="$HOME/.config/yazi"
+WZT="$HOME/.config/wezterm/lua"
+
+THEME="$1"
+THEMES="$HOME/.themes/$THEME"
+
+#if not theme specified
+if [ -z "$THEME" ]; then
+  echo "Usage: theme-switcher <theme-name>"
+  exit 1
+fi
+
+#if not theme found
+if [ ! -d "$THEMES" ]; then 
+  echo "Theme $THEME not found!"
+  exit 1
+fi
+
+#copy Themes
+#ohmyposh
+rm "$OMP/theme.txt"
+cp "$THEMES/ohmyposh/theme.txt" "$OMP/"
+$HOME/.config/OhMyPosh/config/changeTheme.sh
+#Nvim
+rm "$NVIM/current-theme.lua"
+cp "$THEMES/nvim/current-theme.lua" "$NVIM/"
+#yazi
+rm "$YAZI/theme.toml"
+cp "$THEMES/yazi/theme.toml" "$YAZI/"
+#wezterm
+rm "$WZT/colorScheme.lua"
+cp "$THEMES/wezterm/colorScheme.lua" "$WZT/"
+echo "Theme switch to $THEME without issues"
