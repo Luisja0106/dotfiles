8622e2ffb0074798ed468f83b5495cb380b15df2
Luffy <luisjaperez0106@gmail.com>
2025-11-07 13:47:02 -0500

    add: flash nvim plugin
    Added the flash nvim plugin, and config the keymaps of surrounding

--- .config/nvim/lua/Luffy/plugins/mini.lua
+++ .config/nvim/lua/Luffy/plugins/mini.lua
@@ -59,13 +59,13 @@ return {
 			-- saiw surround with no whitespace
 			-- saw surround with whitespace
 			mappings = {
-				add = "sa", -- Add surrounding in Normal and Visual modes
-				delete = "ds", -- Delete surrounding
-				find = "sf", -- Find surrounding (to the right)
-				find_left = "sF", -- Find surrounding (to the left)
-				highlight = "sh", -- Highlight surrounding
-				replace = "sr", -- Replace surrounding
-				update_n_lines = "sn", -- Update `n_lines`
+				add = "ña", -- Add surrounding in Normal and Visual modes
+				delete = "dñ", -- Delete surrounding
+				find = "ñf", -- Find surrounding (to the right)
+				find_left = "ñF", -- Find surrounding (to the left)
+				highlight = "ñh", -- Highlight surrounding
+				replace = "ñr", -- Replace surrounding
+				update_n_lines = "ñn", -- Update `n_lines`
 
 				suffix_last = "l", -- Suffix to search with "prev" method
 				suffix_next = "n", -- Suffix to search with "next" method
