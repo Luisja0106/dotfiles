57aa46ab2a4594b877e2f32ba80fc46b388e911e
Luffy <luisjaperez0106@gmail.com>
2026-05-30 14:01:11 -0500

    feat:some configs more

--- .config/hypr/config/windowrules.lua
+++ .config/hypr/config/windowrules.lua
@@ -1,10 +1,31 @@
 -- common actions
 local rule = hl.window_rule
-local function setWorkspace(init_class, workspace)
+local MatchType = {
+	class = "class",
+	initial_class = "initial_class",
+	title = "title",
+	initial_title = "initial_title",
+}
+
+local function setWorkspace(identificator, workspace, match_type)
+	local criteria = match_type or MatchType.class
 	rule({
-		name = "set " .. init_class .. " to " .. workspace,
-		match = { initial_class = init_class },
+		name = "set " .. identificator .. " to " .. workspace,
+		match = { [criteria] = identificator },
 		workspace = workspace,
+	})
+end
+
+local function setUtilities(identificator, size, match_type)
+	local criteria = match_type or MatchType.class
+	local window_size = size or { "(monitor_w*0.5)", "(monitor_h*0.5)" }
+	rule({
+		name = "set " .. identificator .. " to floating",
+		match = { [criteria] = identificator },
+		float = true,
+		pin = true,
+		center = true,
+		size = window_size,
 	})
 end
 
@@ -32,9 +53,30 @@ rule({
 })
 
 --set app to workspace
-setWorkspace("helium", 1)
-setWorkspace("tidal-hifi", 10)
-setWorkspace("steam", 9)
-setWorkspace("heroic", 9)
-setWorkspace("net.lutris.Lutris", 9)
-setWorkspace("com.obsproject.Studio", 8)
+setWorkspace("helium", 1, MatchType.initial_class)
+setWorkspace("tidal-hifi", 10, MatchType.initial_class)
+setWorkspace("steam", 9, MatchType.initial_class)
+setWorkspace("heroic", 9, MatchType.initial_class)
+setWorkspace("net.lutris.Lutris", 9, MatchType.initial_class)
+setWorkspace("com.obsproject.Studio", 8, MatchType.initial_class)
+
+--utilities apps
+setUtilities("bluetui", nil, MatchType.initial_title)
+setUtilities("org.pulseaudio.pavucontrol", nil, MatchType.initial_class)
+setUtilities("org.nmrs.ui", nil, MatchType.initial_class)
+setUtilities("btop", { "(monitor_w*0.6)", "(monitor_h*0.8)" }, MatchType.initial_title)
+setUtilities("htop", { "(monitor_w*0.6)", "(monitor_h*0.8)" }, MatchType.initial_title)
+
+--specific rules
+
+rule({
+	name = "Picture-In-Picture",
+	match = {
+		initial_title = "Picture in picture",
+	},
+	float = true,
+	pin = true,
+	size = { 701, 394 },
+	move = { 1200, 663 },
+	content = "video",
+})
