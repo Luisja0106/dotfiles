9c04a3283cbde2d82b646e2c63fb7ce7fe82e460
Luffy <luisjaperez0106@gmail.com>
2026-05-30 13:12:53 -0500

    feat: add some windowrules

--- .config/hypr/config/windowrules.lua
+++ .config/hypr/config/windowrules.lua
@@ -1,13 +1,14 @@
-local suppressMaximizeRule = hl.window_rule({
-	-- Ignore maximize requests from all apps. You'll probably like this.
-	name = "suppress-maximize-events",
-	match = { class = ".*" },
+-- common actions
+local rule = hl.window_rule
+local function setWorkspace(init_class, workspace)
+	rule({
+		name = "set " .. init_class .. " to " .. workspace,
+		match = { initial_class = init_class },
+		workspace = workspace,
+	})
+end
 
-	suppress_event = "maximize",
-})
--- suppressMaximizeRule:set_enabled(false)
-
-hl.window_rule({
+rule({
 	-- Fix some dragging issues with XWayland
 	name = "fix-xwayland-drags",
 	match = {
@@ -22,10 +23,18 @@ hl.window_rule({
 	no_focus = true,
 })
 
-hl.window_rule({
+rule({
 	name = "move-hyprland-run",
 	match = { class = "hyprland-run" },
 
 	move = "20 monitor_h-120",
 	float = true,
 })
+
+--set app to workspace
+setWorkspace("helium", 1)
+setWorkspace("tidal-hifi", 10)
+setWorkspace("steam", 9)
+setWorkspace("heroic", 9)
+setWorkspace("net.lutris.Lutris", 9)
+setWorkspace("com.obsproject.Studio", 8)
