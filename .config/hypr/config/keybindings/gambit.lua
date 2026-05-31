63ba30649e10711604a08dd0404d8c741c85d6d5
Luffy <luisjaperez0106@gmail.com>
2026-05-31 15:35:44 -0500

    feat:update some neovim plugins

--- .config/hypr/config/keybindings/gambit.lua
+++ .config/hypr/config/keybindings/gambit.lua
@@ -5,17 +5,17 @@ hl.bind(toggleGambit, hl.dsp.submap("gambit"))
 
 -- Start a submap called "resize".
 hl.define_submap("gambit", function()
-	local function gambitBind(key, action, description)
-		hl.bind(mainMod .. " + " .. key, action, { description = description })
-	end
-	gambitBind("K", hl.dsp.exec_cmd("pamixer -i 2"), "Increase volume")
-	gambitBind("K", hl.dsp.exec_cmd("pamixer -d 2"), "Decrease volume")
-	gambitBind("S", hl.dsp.exec_cmd("playerctl play-pause "), "Pause/Play music")
-	gambitBind("L", hl.dsp.exec_cmd("playerctl next "), "next song")
-	gambitBind("H", hl.dsp.exec_cmd("playerctl previous "), "previous song")
-	gambitBind("M", hl.dsp.exec_cmd("swaync-client -d -sw"), "Toggle notifications")
-	gambitBind("G", hl.dsp.exec_cmd("~/.config/hypr/scripts/toggle_gamemode.sh"), "Toggle Gamemode")
+	hl.bind(mainMod .. " + K", hl.dsp.exec_cmd("pamixer -i 2"), { description = "Increase volume", repeating = true })
+	hl.bind(mainMod .. " + J", hl.dsp.exec_cmd("pamixer -d 2"), { description = "Decrease volume", repeating = true })
+	hl.bind(mainMod .. " + S", hl.dsp.exec_cmd("playerctl play-pause "), { description = "Pause/Play music" })
+	hl.bind(mainMod .. " + L", hl.dsp.exec_cmd("playerctl next "), { description = "next song" })
+	hl.bind(mainMod .. " + H", hl.dsp.exec_cmd("playerctl previous "), { description = "previous song" })
+	hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("swaync-client -d -sw"), { description = "Toggle notifications" })
+	hl.bind(
+		mainMod .. " + G",
+		hl.dsp.exec_cmd("~/.config/hypr/scripts/toggle_gamemode.sh"),
+		{ description = "Toggle Gamemode" }
+	)
 
-	hl.bind("escape", hl.dsp.submap("reset"))
-	hl.bind("SPACE", hl.dsp.submap("reset"))
+	hl.bind("escape", hl.dsp.submap("reset"), { description = "Exit submap" })
 end)
