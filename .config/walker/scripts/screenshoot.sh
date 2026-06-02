294e77e3556586507f1ac2f6a4c70738773208aa
Luffy <luisjaperez0106@gmail.com>
2026-06-01 23:42:44 -0500

    feat: fix the issue that walker menu appears in the picture

--- .config/walker/scripts/screenshoot.sh
+++ .config/walker/scripts/screenshoot.sh
@@ -2,7 +2,7 @@
 
 option_screen="All Screen\nActive Window\nSelected Area"
 option_save="Copy to Clipboard\nSave"
-walker_cmd="walker --dmenu -n -N -H --maxheight 200"
+walker_cmd="walker --dmenu -n -N -H -e --maxheight 200"
 chosen_screen=$(echo -e $option_screen | $walker_cmd)
 
 if [[ -z "$chosen_screen" ]]; then
@@ -28,5 +28,7 @@ case "$chosen_save" in
     "Copy to Clipboard") flag_save="--clipboard-only" ;;
     "Save")              flag_save="" ;;
 esac
+
+sleep 0.7
 
 hyprshot $flag_screen $flag_save
