fb7447e207469b5cfe99553f1d18133d845938a7
Luffy <luisjaperez0106@gmail.com>
2026-07-31 16:39:24 -0500

    feat: add herdr and his config

--- .config/nvim/lua/Luffy/plugins/vim-tmux.lua
+++ .config/nvim/lua/Luffy/plugins/vim-tmux.lua
@@ -1,4 +1,12 @@
 return {
 	"Christoomey/vim-tmux-navigator",
 	event = "VeryLazy",
+	init = function()
+		vim.g.tmux_navigator_no_mappings = 1
+	end,
+	config = function()
+		dofile(
+			vim.fn.expand("/home/luffy/.config/herdr/plugins/github/vim-herdr-navigation-a8bf42123d81/editor/nvim.lua")
+		)
+	end,
 }
